3eebd720676ca62863b97f756938cbcc1d1558e0
vaibhavahbiav <mahawarv97@gmail.com>
2025-11-07 11:31:28 +0530

    reverting some changes

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators:false,
-  output: "export", // ✅ enable static export
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: process.env.NODE_ENV === "production" ? "/next-thing/" : "",
-  basePath: process.env.NODE_ENV === "production" ? "/next-thing" : "",
 };
 
 export default nextConfig;
